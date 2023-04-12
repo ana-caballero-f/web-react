b605eaa6152265ba851f7994ff55d659351bf29c
ana caballero <anacaballerof@hotmail.com>
2023-04-12 09:42:34 +0200

    formulario para recibir email

--- src/components/FormularioComponent/FormularioComponent.js
+++ src/components/FormularioComponent/FormularioComponent.js
@@ -2,11 +2,53 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './FormularioComponent.module.scss';
 
-const FormularioComponent = () => (
-  <div className={styles.FormularioComponent}>
-    FormularioComponent Component
-  </div>
-);
+import emailjs from "@emailjs/browser";
+import { useRef } from 'react';
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+
+
+const FormularioComponent = () => {
+
+  const form = useRef();
+  const MySwal= withReactContent(Swal)
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+    .sendForm('service_npt7czb', 'template_9x2yos7', form.current, 'oCARw5ggzhnvntNQ2')
+      .then((result) => {
+          console.log(result.text);
+          e.target.reset()
+          MySwal.fire({
+            title: <p>¡Gracias por contactar! En breve recibirás la información solicitada</p>
+          })
+      }, 
+      (error) => {
+          console.log(error.text);
+      });
+  };
+
+
+  
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Nombre</label>
+      <input placeholder='Escribe tu nombre' type="text" name="name" required/>
+      <label>Apellidos</label>
+      <input placeholder='Escribe tus apellidos' type="text" name="surname" required/>
+      <label>Email</label>
+      <input placeholder='Escribe tu correo' type="email" name="email" required/>
+      <label>Teléfono</label>
+      <input placeholder='Escribe tu teléfono' type="text" name="telefono" required/>
+      <label>Quiero información sobre ...</label>
+      <textarea placeholder='Dime el tipo de sesión' name="mensaje" required/>
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
+ 
 
 FormularioComponent.propTypes = {};
 
--- src/pages/ContactoPage/ContactoPage.js
+++ src/pages/ContactoPage/ContactoPage.js
@@ -1,10 +1,30 @@
+import { useState } from "react";
+
+import FormularioComponent from "../../components/FormularioComponent/FormularioComponent"
 
 
-export default function HomePage() {
+
+
+export default function ContactoPage() {
+
+    // const initialState = { name: "", surname: "", email: "" }
+    // const [userData, setUserData] = useState(initialState)
+  
+    // function changeData(e) {
+    //   setUserData({ ...userData, [e.target.name]: e.target.value })
+    // }
+
 
     return(
-       <div>
-            <h1> Contacto page</h1>
-       </div> 
-    )
-}
+      <div>
+        <h1>Formulario de contacto</h1>
+        <p>Rellena el formulario y me pondré en contacto contigo para darte la información solicitada</p>
+        <section>
+          <FormularioComponent></FormularioComponent>
+        </section>
+      </div>  
+     
+  );
+};
+
+    
